bb33f3d4556bf302c3ada2f985d468ef7ae36331
Eloy Chang <eloy.chang.182@gmail.com>
2024-10-09 16:08:21 -0300

    package deployed

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='package_name',
-    version='0.0.0',
+    name='format_logger',
+    version='0.0.4',
     author='Eloy Chang',
     author_email="echang.epsilondl@gmail.com",
     description='Description.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/echang1802/normandy",
+    url="https://github.com/echang1802/format-logger",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
